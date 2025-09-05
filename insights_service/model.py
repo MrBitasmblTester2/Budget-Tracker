5eccbd1ce177d264c5577e1f2403547ec12262cd
MrBitasmblTester2 <lulu2214@proton.me>
2025-09-05 19:27:38 +0400

    Add model.py

---
+++ insights_service/model.py
@@ -0,0 +1,8 @@
+# model.py snippet
+# import torch
+# class SpendingModel(torch.nn.Module):
+#     def forward(self, x): return x
+# def load_model(path:str):
+#     model = SpendingModel()
+#     # model.load_state_dict(torch.load(path))
+#     return model
