34dea507b123fac26264d90d35fa03231902ba8a
MrBitasmblTester2 <lulu2214@proton.me>
2025-09-05 19:27:36 +0400

    Add CategoryService.cs

---
+++ BudgetTracker/Services/CategoryService.cs
@@ -0,0 +1,5 @@
+// Services/CategoryService.cs snippet
+// public interface ICategoryService { string ApplyCategory(Transaction tx); }
+// public class CategoryService : ICategoryService {
+//   public string ApplyCategory(Transaction tx) => tx.CustomCategory ?? "Uncategorized";
+// }
