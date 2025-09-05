cb29a220caf8df70bf7068ad2a32a550aeea0677
MrBitasmblTester2 <lulu2214@proton.me>
2025-09-05 19:27:35 +0400

    Add Transaction.cs

---
+++ BudgetTracker/Models/Transaction.cs
@@ -0,0 +1,7 @@
+// Models/Transaction.cs snippet
+// public class Transaction {
+//   public int Id { get; set; }
+//   public decimal Amount { get; set; }
+//   public string Category { get; set; }
+//   public string CustomCategory { get; set; }
+// }
