a45e2d05310fa9192956bae4114179de1caa3ca5
MrBitasmblTester2 <lulu2214@proton.me>
2025-09-05 19:27:33 +0400

    Add Program.cs

---
+++ BudgetTracker/Program.cs
@@ -0,0 +1,7 @@
+// Program.cs snippet
+// var builder = WebApplication.CreateBuilder(args);
+// builder.Services.AddControllers();
+// builder.Services.AddHttpClient();
+// var app = builder.Build();
+// app.MapControllers();
+// app.Run();
